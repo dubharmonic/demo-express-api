e396a2d4b0b229a44034e3d45ab8de3d5779c032
Eric Sorenson <eric@sorenson.software>
2018-01-02 10:24:37 -0600

    cors fix

--- src/server.js
+++ src/server.js
@@ -22,6 +22,7 @@ server.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   next();
 });
 
